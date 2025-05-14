481bac72166b46624c111ef5d9cae17e7e236ed7
fethi bey abi ayad <Fethi.Bey.Abi.Ayad.ccsmtl@ssss.gouv.qc.ca>
2025-05-14 07:55:43 -0400

    disable initializer

--- assets/javascripts/discourse/components/avatar-selector.js
+++ assets/javascripts/discourse/components/avatar-selector.js
@@ -1,20 +1,52 @@
-import Component from "@glimmer/component";
-import { ajax } from "discourse/lib/ajax";
-import { inject as service } from "@ember/service";
+import Component from "@ember/component";
+import hbs from "discourse/widgets/hbs-compiler";
 
-export default class AvatarSelectorComponent extends Component {
-  @service currentUser;
+export default Component.extend({
+  layout: hbs`
+    <div class="custom-avatar-selector">
+      <label for="custom-avatar-select">Choisir un avatar :</label>
+      <select id="custom-avatar-select">
+        {{#each avatars as |avatar|}}
+          <option value="{{avatar.url}}">{{avatar.name}}</option>
+        {{/each}}
+      </select>
+    </div>
+  `,
 
-  avatars = [
-    "/images/avatars/avatar1.png",
-    "/images/avatars/avatar2.png",
-    "/images/avatars/avatar3.png"
-  ];
+  avatars: null,
 
-  selectAvatar(url) {
-    ajax(`/u/${this.currentUser.username}/preferences/avatar/pick`, {
-      type: "PUT",
-      data: { url },
-    }).then(() => location.reload());
+  didInsertElement() {
+    this._super(...arguments);
+
+    // Liste d'avatars (tu peux les générer dynamiquement plus tard)
+    this.set("avatars", [
+      { name: "Avatar 1", url: "/images/avatars/avatar1.png" },
+      { name: "Avatar2", url: "/images/avatars/avatar2.png" },
+      { name: "Avatar3", url: "/images/avatars/avatar3.png" }
+    ]);
+
+    // Ajout du listener de sélection
+    const select = this.element.querySelector("#custom-avatar-select");
+    if (select) {
+      select.addEventListener("change", (event) => {
+        const selectedUrl = event.target.value;
+        console.log("[custom-avatars] Avatar sélectionné :", selectedUrl);
+
+        // Requête vers le backend pour changer l’avatar
+        fetch("/u/avatar-picker", {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+            "X-CSRF-Token": document.querySelector("meta[name=csrf-token]").content
+          },
+          body: JSON.stringify({
+            upload_id: null,
+            url: selectedUrl
+          })
+        }).then(() => {
+          window.location.reload();
+        });
+      });
+    }
   }
-}
+});
--- assets/javascripts/discourse/initializers/add-avatar-selector.js
+++ assets/javascripts/discourse/initializers/add-avatar-selector.js
@@ -3,10 +3,71 @@ import { withPluginApi } from "discourse/lib/plugin-api";
 export default {
   name: "add-avatar-selector",
 
-  initialize(container) {
-    withPluginApi("0.11.3", (api) => {
-      api.decorateWidget("user-preferences:after-avatar-uploader", (helper) => {
-        return helper.attach("avatar-selector");
+  initialize() {
+    console.log("[custom-avatars] Initializer loaded");
+
+    withPluginApi("1.8.0", (api) => {
+      console.log("[custom-avatars] Plugin API available");
+
+      api.modifyClass("component:avatar-uploader", {
+        didInsertElement() {
+          this._super(...arguments);
+          console.log("[custom-avatars] avatar-uploader remplacé");
+          console.log(this.element);
+          
+          console.log("Args", this.currentUser);
+          const userId  = this.currentUser.id;
+
+          // this.element.innerHTML = "";
+
+          // const wrapper = document.createElement("div");
+          // wrapper.id = "custom-avatar-selector-outlet";
+          // this.element.appendChild(wrapper);
+
+          // const avatars = [
+          //   { name: "Avatar 1", url: "/images/avatars/avatar1.png" },
+          //   { name: "Avatar 2", url: "/images/avatars/avatar2.png" },
+          //   { name: "Avatar 3", url: "/images/avatars/avatar3.png" }
+          // ];
+
+          // const selectorHtml = `
+          //   <div class="custom-avatar-selector">
+          //     <label for="custom-avatar-select">Choisir un avatar :</label>
+          //     <select id="custom-avatar-select">
+          //       ${avatars
+          //         .map(
+          //           (avatar) =>
+          //             `<option value="${avatar.url}">${avatar.name}</option>`
+          //         )
+          //         .join("")}
+          //     </select>
+          //   </div>
+          // `;
+
+          // wrapper.innerHTML = selectorHtml;
+
+          // const select = wrapper.querySelector("#custom-avatar-select");
+          // if (select) {
+          //   select.addEventListener("change", (event) => {
+          //     const selectedUrl = event.target.value;
+          //     console.log("[custom-avatars] Avatar sélectionné :", selectedUrl);
+
+          //     fetch(`/u/${userId}/avatar-picker`, {
+          //       method: "PUT",
+          //       headers: {
+          //         "Content-Type": "application/json",
+          //         "X-CSRF-Token": document.querySelector("meta[name=csrf-token]").content
+          //       },
+          //       body: JSON.stringify({
+          //         upload_id: null,
+          //         url: selectedUrl
+          //       })
+          //     }).then(() => {
+          //       //window.location.reload();
+          //     });
+          //   });
+          // }
+        }
       });
     });
   },
